7b6fb9b1110dfdf6e065d27a02a2067ec980c391
chethan.m <chethan.m@arithaconsulting.com>
2024-02-08 17:39:56 +0530

    updated to latest editor

--- src/components/HeroBrikThree.jsx
+++ src/components/HeroBrikThree.jsx
@@ -1,42 +1,41 @@
 import React from "react";
-import { Box,Title,Text,Button} from "@apf/core";
+import { Title,Text,Button} from "@apf/core";
 import "@apf/core/dist/style.css";
 import "./HeroBrikThree.css";
 function HeroThree(props){
     // eslint-disable-next-line react/prop-types
-    const backgroundImg = props.data.data.find((item) => item.content.type === "image" && item.content.for === "background");
+    const titleObject = props?.data?.data?.find((item) => item?.content?.type === "title" && item?.content?.for === "main title");
     // eslint-disable-next-line react/prop-types
-    const hoverImg = props.data.data.find((item) => item.content.type === "image" && item.content.for === "hover");
+    const BackgroundImage = props?.data?.data?.find((item) => item?.content?.type === "img" && item?.content?.for === "Background Image");
     // eslint-disable-next-line react/prop-types
-    const titleObj = props.data.data.find((item) => item.content.type === "title");
+    const ButtonOne = props?.data?.data?.find((item) => item?.content?.type === "button" && item?.content?.for === "button One");
     // eslint-disable-next-line react/prop-types
-    const textObj = props.data.data.find((item) => item.content.type === "text");
+    const ButtonTwo = props?.data?.data?.find((item) => item?.content?.type === "button" && item?.content?.for === "button Two");
     // eslint-disable-next-line react/prop-types
-    const buttonObj = props.data.data.find((item) => item.content.type === "button");
-    return(
-    <div className="containor">
-        <Box>
-            <img src={backgroundImg.content.link}  className="HeroImageDiv"></img>
-                    <div className="ChildImage">
-                        <img src={hoverImg.content.link} className="ChildImageTag"></img>
-                        
-                    </div>
-                    <div className="ParentContainerContentDiv">
-                        <div className="ParentTitleDiv">
-                            <Title style={{ fontFamily: titleObj.style.font.family }} c={titleObj.style.font.color} fz={titleObj.style.font.size} fw={titleObj.style.font.weight} lh={titleObj.style.lineHeight} ta="right">
-                               { titleObj.content.value}
-                            </Title>
-                        </div>
+    const textObject = props.data.data.find((item) => item.content.type === "subtext" && item?.content?.for === "subText");
+    // eslint-disable-next-line react/prop-types
+    const BrikImage = props?.data?.data?.find((item) => item?.content?.type === "img" && item?.content?.for === "Brik Image");
 
-                        <div className="ChildContainerTextDiv">
-                            <Text  style={{ fontFamily: textObj.style.font.family }} color={textObj.style.font.color} ta="right" fz={textObj.style.font.size} fw={textObj.style.font.weight} lh={textObj.style.lineHeight}> {textObj.content.value}</Text>
-                        </div>
-                        <div className="ParentButtonDiv">
-                            <Button style={{ color: buttonObj.style.font.color, backgroundColor: buttonObj.style.backgroundColor ,fontFamily: buttonObj.style.font.family}}  size={buttonObj.style.font.size} variant="filled" radius="xl" className="ChildButtonDiv">{buttonObj.content.value}</Button>
-                        </div>
-                    </div>
-            
-        </Box>
+    return(
+    <div className="Hero-containor">
+        <div className="Hero-Background-Image">
+            <img src={BackgroundImage?.content?.src} className="Hero-Background-Image-Tag"></img>
+        </div>
+        <div className="Hero-containor-content">
+            <div className="Hero-Title">
+                <Title c={titleObject?.style?.font?.color} fz={titleObject?.style?.font?.size}  style={{ fontFamily: titleObject?.style?.font?.family }} fw={titleObject?.style?.font?.weight} className="Title-Tag">{titleObject?.content?.value}</Title>
+            </div>
+            <div className="Hero-Buttons-Container">
+                <Button style={{ color: ButtonOne?.style?.font.color, backgroundColor: ButtonOne?.style?.backgroundColor ,fontFamily: ButtonOne?.style.font.family, height:ButtonOne?.style?.height, width:ButtonOne?.style?.width}}  fz={ButtonOne?.style.font.size} fw={ButtonOne?.style.font.weight} variant="filled" radius={ButtonOne?.style?.radius}>{ButtonOne?.content?.value}</Button>
+                <Button style={{ color: ButtonTwo?.style?.font.color, backgroundColor: ButtonTwo?.style?.backgroundColor ,fontFamily: ButtonTwo?.style.font.family, height:ButtonTwo?.style?.height, width:ButtonTwo?.style?.width}}  fz={ButtonTwo?.style.font.size} fw={ButtonTwo?.style.font.weight}variant="filled" radius={ButtonTwo?.style?.radius}>{ButtonTwo?.content?.value}</Button>
+            </div>
+            <div className="Hero-Text">
+                <Text c={textObject?.style?.font.color} fz={textObject?.style?.font?.size} style={{ fontFamily: textObject?.style?.font?.family}} fs={textObject?.style?.font?.style} fw={textObject?.style?.font?.weight}>{textObject?.content?.value}</Text>
+            </div>
+            <div className="Hero-Image">
+                <img src={BrikImage?.content?.src}  className="Hero-Image-Tag" ></img>
+            </div>
+        </div>
     </div>
     );
 }
